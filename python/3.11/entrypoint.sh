6dc5490aa75f63de1a7358ed5639e949fd354626
Vladimir nett00n Budylnikov <git@nett00n.org>
2024-07-08 13:06:03 +0400

    2024-07-08 13:06:03+04:00

--- python/3.11/entrypoint.sh
+++ python/3.11/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dropbear -R
-pip install -r /home/nett00n/personal-env/dev-container/requirements.txt --user
+pip install -r /home/${USERNAME}/personal-env/dev-container/requirements.txt --user
 sudo apt-get update -qq
 sleep infinity
